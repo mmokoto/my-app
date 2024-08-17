89704fafb20f19ac795ea868e250cdf17c7c40d6
mmokoto <miranda.mokoto@outlook.com>
2024-08-17 16:46:04 +0200

    added a new footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App"> Weather App </div>;
+  return (
+  <div className="App">
+     Weather App
+      <footer>
+      This proect was coded by Miranda Mokoto
+     <a 
+  href="https://github.com/mmokoto/my-app"
+  target="_blank">
+</a>
+
+     </footer>
+      </div>
+      );
 }
 
  
