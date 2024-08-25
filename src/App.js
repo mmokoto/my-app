cefda01fc950b7e7acfa9bfa115e65edf505b023
mmokoto <miranda.mokoto@outlook.com>
2024-08-25 17:14:10 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 export default function App() {
   return (
   <div className="App">
+    <div className="container">
      <h1>Weather App </h1>
       <footer>
-      This project was coded by Miranda Mokoto and is {" "}
-     <a 
-  href="https://github.com/mmokoto/my-app"
-  target="_blank"
-    rel="noopener noreferrer"
-  >open-sourced on Github</a>
+      This project was coded by {" "} and is 
+     <a  href="https://github.com/mmokoto/my-app" target="_blank">
+    
+  
+  open-sourced on Github
+  
+  </a>
 
      </footer>
       </div>
+</div>
       );
 }
 
