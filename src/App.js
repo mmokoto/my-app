96e03de09df26e9260f56b4fc9e6b53054e04f72
mmokoto <miranda.mokoto@outlook.com>
2024-09-01 12:47:27 +0200

    made new changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import React from "react";
-import Weather from "./Weather";
+import React from 'react';
 import './App.css';
 
-export default function App() {
+function App() {
   return (
-  <div className="App">
-    <div className="container">
-     <h1>Weather App </h1>
-     <Weather />
-      <footer>
-      This project was coded by {" "} 
-     <a  href="https://github.com/mmokoto/my-app" target="_blank">
-        Miranda Mokoto
-  </a>{" "}
-       and is{" "}
-  
-  <a href="https://github.com/mmokoto/my-app" target="_blank"
-  >
-  open-sourced on Github
-  </a>
-
-     </footer>
-      </div>
-</div>
-      );
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to React Weather App</h1>
+      </header>
+    </div>
+  );
 }
 
- 
+export default App;
